c9af04d11ae23195a0cd04b8d89d57b17ce17f60
Luís <77413441+Luis-Spessoto@users.noreply.github.com>
2024-10-16 22:12:40 -0300

    Update TstObj.java

--- POO.15.10/TstObj.java
+++ POO.15.10/TstObj.java
@@ -7,24 +7,22 @@ public class TstObj{
 
 		caneta.setCor("Azul");
 		caneta.setValor(5.99);
+		caneta.getEnder().setRua("Das Flores"); //getEnder recupera classe ender e o set define o dado a ser passado dentro do que exite dentro de ender = reflections
+		caneta.getEnder().setNum(115);
+
 		giz.setCor("Vermelho");
 		giz.setValor(3.99);
 
 	
 		System.out.println("\nCor da caneta: "+ caneta.getCor());
 		System.out.println("\nValor da caneta: "+ caneta.getValor());
+
 		System.out.println("\nCor do giz: "+ giz.getCor());
 		System.out.println("\nValor do giz: "+ giz.getValor());
 
 
-		
-		Endereco utf = new Endereco();
-
-		utf.setRua("Av Alberto Carazzai");
-		utf.setNum(1640);
-
-		System.out.println("\nRua da UTF: "+ utf.getRua());
-		System.out.println("\nNumero da UTF: "+ utf.getNum());
+		System.out.println("\nRua da UTF: "+ ender.getEnder().getNum()); //reflexividade tbm
+		System.out.println("\nNumero da UTF: "+ ender.getEnder().getNum());
 
 
 	}
